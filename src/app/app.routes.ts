ee40ea3b518f3ca386b7585aafa6ada6b8031232
Harshal6354 <harshaldhanavade@gmail.com>
2025-03-20 13:50:20 +0530

    upadate ngcontainer component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,13 +4,18 @@ import { AppComponent } from './app.component';
 import { NgContainerComponent } from './pages/ng-container/ng-container.component';
 import { NgT2Component } from './pages/ng-t2/ng-t2.component';
 import { Ngc2Component } from './pages/ngc2/ngc2.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
 
 export const routes: Routes = [
 
     {
         path: '',
-        redirectTo: 'app',
+        redirectTo: '',
         pathMatch: 'full'
+    },
+    {
+        path:'navbar',
+        component:NavbarComponent
     }
     ,
     {
@@ -18,10 +23,7 @@ export const routes: Routes = [
         component: TempComponent
     }
     ,
-    {
-        path: 'app',
-        component: AppComponent
-    },
+    
     {
         path: 'ng-con',
         component: NgContainerComponent
--- src/app/pages/ng-container/ng-container.component.ts
+++ src/app/pages/ng-container/ng-container.component.ts
@@ -10,19 +10,26 @@ import { Component, inject, OnInit } from '@angular/core';
   styleUrl: './ng-container.component.css'
 })
 export class NgContainerComponent  implements OnInit{
-  ngOnInit(): void {
+  ngOnInit() {
     this.getUser();
+    
   }
  isvisible:boolean=true;
  api_key='https://projectapi.gerasim.in/api/CollegeProject/getProjectByUser'
 
  http=inject(HttpClient)
 userlist:any[]=[]
+isApi:Boolean=false;
  
  getUser(){
+  this.isApi=true;
   this.http.get(`${this.api_key}`).subscribe((res:any)=>{
     this.userlist=res;
+    this.isApi=false;
+    
   })
-  console.log("error")
  }
+
+ 
+
 }
