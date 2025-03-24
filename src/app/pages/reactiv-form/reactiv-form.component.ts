1b3a9ec889a0b35972cee46590703dd7113fd1bf
Harshal6354 <harshaldhanavade@gmail.com>
2025-03-24 15:47:22 +0530

    Reactive form

---
+++ src/app/pages/reactiv-form/reactiv-form.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormControl, FormControlName, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactiv-form',
+  standalone: true,
+  imports: [ReactiveFormsModule,FormsModule],
+  templateUrl: './reactiv-form.component.html',
+  styleUrl: './reactiv-form.component.css'
+})
+export class ReactivFormComponent {
+ 
+  studentData:FormGroup=new FormGroup({
+    firstname:new FormControl("",[Validators.required,Validators.minLength(4)]),
+    secondname:new FormControl(""),
+    username:new FormControl("",[Validators.email]),
+    city:new FormControl(""),
+    state:new FormControl(""),
+    zipcode:new FormControl(""),
+    isAcceptable:new FormControl(""),
+  })
+  submit:unknown;
+  onsubmit(){
+    debugger;
+    this.submit=this.studentData.value;
+  }
+}
